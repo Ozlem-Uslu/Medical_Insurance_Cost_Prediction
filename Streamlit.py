b5996fc40bcc49d54599696540c4698c8aa8acb6
Ozlem-Uslu <ozlmzsln@gmail.com>
2024-05-15 14:15:43 +0200

    add

--- Streamlit.py
+++ Streamlit.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import joblib
 
 
 st.set_page_config(layout="wide")   #görüntünün tüm ekrani kaplamasi icin
